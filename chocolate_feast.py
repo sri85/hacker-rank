32c6ef46c61b190a01f50c4446478ba8df5b2507
Sripathi Pai <sri85@users.noreply.github.com>
2016-05-31 17:23:53 +0100

    Solution for chocolate feast

---
+++ chocolate_feast.py
@@ -0,0 +1,15 @@
+# Problem statement: https://www.hackerrank.com/challenges/chocolate-feast
+T = int(input())
+for _ in xrange(T):
+    n,c,m = raw_input().strip().split(' ')
+    n,c,m = [int(n),int(c),int(m)]
+    total = wrappers = n / c;
+
+    while wrappers >= m:
+	 
+        trade = wrappers / m
+        total +=  trade
+        wrappers = wrappers % m
+        wrappers = wrappers + trade
+
+    print total
